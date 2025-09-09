83cb4a20ec15066a5302e2b400366096b86e7344
Raj Shekokar <rajshekokar3@gmail.com>
2025-09-09 19:48:21 +0530

    Project structure ready

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools  import setup,find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author="Raj Shekokar",
+    author_email="rajshekokar3@gmail.com",
+    packages=find_packages())
